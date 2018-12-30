7fb7914906cfef4ec855e10dd8e70c80ce2bdafd
sjoerdschelhaas <sjoerdschelhaas.ss@gmail.com>
2018-12-30 16:29:40 +0100

    Heb nog niks hier aan gedaan

--- core/src/com/mygdx/game/Menu.java
+++ core/src/com/mygdx/game/Menu.java
@@ -1,8 +1,21 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 public class Menu {
     // TODO add a menu
-    public Menu(){
+    Ball game;
+    Batch batch;
+    Camera camera;
+    Viewport viewport;
 
+    public Menu(Ball game){
+        this.game = game;
+        batch = game.batch;
+        camera = game.camera;
+        viewport = game.viewport;
+        game.resize(game.screenWidth, game.screenHeight); //updates the viewport.
     }
 }
